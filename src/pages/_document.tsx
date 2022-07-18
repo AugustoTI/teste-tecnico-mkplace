2da246700613034e228f4ee1ce1bd5b7566a399a
AugustoTI <augustocesarti2021@gmail.com>
2022-07-18 19:40:36 -0300

    refactor: basic organization

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+function Document() {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/favicon-mkplace.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,6 +1,6 @@
-import { IncomingForm } from "formidable";
-import type { NextApiRequest, NextApiResponse } from "next";
-import fs from "fs";
+import { IncomingForm } from 'formidable';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import fs from 'fs';
 
 type Data = {
   url: string;
@@ -14,10 +14,10 @@ export const config = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | { message: string }>
+  res: NextApiResponse<Data | { message: string }>,
 ) {
   try {
-    if (req.method === "POST") {
+    if (req.method === 'POST') {
       const { files } = await new Promise((resolve, reject) => {
         const form = new IncomingForm({
           keepExtensions: true,
@@ -31,17 +31,17 @@ export default async function handler(
 
       if (!files?.file?.filepath) {
         throw new Error(
-          "Ocorreu algo com o arquivo que vc enviou, pois não recebemos ele"
+          'Ocorreu algo com o arquivo que vc enviou, pois não recebemos ele',
         );
       }
 
       if (files?.file?.size > 1000000) {
         throw new Error(
-          "Arquivo muito grande, só é possível enviar arquivos de até 1MB"
+          'Arquivo muito grande, só é possível enviar arquivos de até 1MB',
         );
       }
 
-      const baseDir = "./public/uploads/";
+      const baseDir = './public/uploads/';
       const oldPath = files.file.filepath;
       const newPath = `${baseDir}${files.file.originalFilename}`;
 
@@ -57,7 +57,7 @@ export default async function handler(
 
       res.status(200).json({ url: newPath });
     } else {
-      throw new Error("Método inválido");
+      throw new Error('Método inválido');
     }
   } catch (error: any) {
     res.status(400).json({ message: error?.message || error });
