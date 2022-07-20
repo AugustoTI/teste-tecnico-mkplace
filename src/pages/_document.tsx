4282b16b34ea43066bda46aca6c5dd48434d07fc
AugustoTI <augustocesarti2021@gmail.com>
2022-07-20 11:49:21 -0300

    add: website fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,12 @@ function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700;800&family=Montserrat:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon-mkplace.ico" />
       </Head>
       <body>
